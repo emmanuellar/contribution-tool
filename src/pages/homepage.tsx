e27b59c327b232701c1ab15f2cc465d660de788e
Clément Biron <clement.biron@gmail.com>
2022-02-17 10:05:14 +0100

    Better homepage wording

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,17 +1,18 @@
 import Search, { SearchProps } from 'components/Search/Search';
+import { WithI18nResult, withI18n } from 'modules/I18n';
 
 import Column from 'modules/Common/components/Column';
 import Container from 'modules/Common/containers/Container';
 import Hero from 'modules/Common/components/Hero';
 import { FiAlertTriangle as IconAlert } from 'react-icons/fi';
 import Layout from 'modules/Common/containers/Layout';
+import { MDXRemote } from 'next-mdx-remote';
 import TextContent from 'modules/Common/components/TextContent';
 import { useEvent } from 'react-use';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
-import { withI18n } from 'modules/I18n';
 
-const HomePage = () => {
+const HomePage = ({ mdxContent }: WithI18nResult) => {
   const { t } = useTranslation();
   const router = useRouter();
   const { localPath, destination, versionsRepo } = router.query;
@@ -54,7 +55,7 @@ const HomePage = () => {
           <Container gridCols="12" gridGutters="11" flex={true} paddingX={false}>
             <Column width={100}>
               <TextContent>
-                <p>{t('homepage:content.p1')}</p>
+                <MDXRemote {...(mdxContent as any)} />
               </TextContent>
               <Search
                 label={t('homepage:search.label')}
@@ -70,8 +71,10 @@ const HomePage = () => {
   );
 };
 
-export const getStaticProps = withI18n()(async (props: any) => {
-  return JSON.parse(JSON.stringify({ props: { ...props }, revalidate: 10 }));
-});
+export const getStaticProps = withI18n({ load: 'mdx', filename: 'homepage' })(
+  async (props: any) => {
+    return JSON.parse(JSON.stringify({ props: { ...props }, revalidate: 10 }));
+  }
+);
 
 export default HomePage;
