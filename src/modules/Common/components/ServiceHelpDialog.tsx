801e5ed24d1ad4dbda3ed1503540a0037cfcfe6a
Martin Ratinaud <martinratinaud@gmail.com>
2022-05-26 13:03:08 +0400

    Refactor service dialog help in separate file

---
+++ src/modules/Common/components/ServiceHelpDialog.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import classNames from 'classnames';
+import sDialog from './Dialog.module.css';
+import { Dialog } from '@headlessui/react';
+import TextContent from 'modules/Common/components/TextContent';
+import { Trans } from 'react-i18next';
+import Button from 'modules/Common/components/Button';
+import { useTranslation } from 'next-i18next';
+
+type ServiceHelpDialogProps = {
+  open: boolean;
+  onClose: any;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const ServiceHelpDialog: React.FC<ServiceHelpDialogProps> = ({ onClose, open }) => {
+  const { t } = useTranslation();
+  return (
+    <Dialog open={open} as="div" className={classNames(sDialog.dialog)} onClose={onClose}>
+      <Dialog.Overlay className={classNames(sDialog.dialog_overlay)} />
+
+      <div className={classNames(sDialog.dialog_content)}>
+        <Dialog.Title as="h3">{t('service:dialog.start.title')}</Dialog.Title>
+        <Dialog.Description>
+          <TextContent>
+            <p>
+              <Trans i18nKey="service:dialog.start.p1">
+                Most of the time, contractual documents contains a header, a footer, navigation
+                menus, possibly ads… We aim at tracking only{' '}
+                <strong>the significant parts of the document</strong>
+              </Trans>
+            </p>
+            <p>
+              <Trans i18nKey="service:dialog.start.p2">
+                In order to achieve that, you will have to select those specific parts and remove
+                the insignificant ones.
+              </Trans>
+            </p>
+          </TextContent>
+        </Dialog.Description>
+        <div className="mt__L text__right">
+          {/* <Button onClick={toggleDialogOpen}>{t('service:dialog.start.cta')}</Button> */}
+          <Button onClick={onClose}>{t('service:dialog.start.cta')}</Button>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default ServiceHelpDialog;
