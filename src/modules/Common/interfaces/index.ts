059d63d8a99e2159d26d4e8004096f358d345d27
Martin Ratinaud <martinratinaud@gmail.com>
2022-05-26 14:07:21 +0400

    Add API endpoint to verify content of snapshot and version
    based on the config passed

--- src/modules/Common/interfaces/index.ts
+++ src/modules/Common/interfaces/index.ts
@@ -1,4 +1,13 @@
+import { CommonResponse } from 'interfaces';
+
 export interface GetServiceResponse {
   status: 'ok' | 'ko';
   message?: string;
+}
+
+export interface GetServiceVerifyResponse extends CommonResponse {
+  error?: string;
+  snapshot?: string;
+  version?: string;
+  mimeType?: string;
 }
