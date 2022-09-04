f6550de94da144e03fc29343749b5840858d8bc8
Matti Schneider <matti@opentermsarchive.org>
2022-09-04 14:45:42 +0200

    Increase visibility of inspection link

--- src/modules/Contribute/managers/ServiceManager.ts
+++ src/modules/Contribute/managers/ServiceManager.ts
@@ -60,8 +60,10 @@ export const addService = async ({
     '  - **clean**: it does not contain navigation links, unnecessary images, or extra content.',
   ];
 
-  const body = `This suggestion has been created through the [Contribution Tool](https://github.com/OpenTermsArchive/contribution-tool/), which enables graphical declaration of documents. You can see this declaration suggestion [online](${url}) or [on your local instance](${localUrl}) if you have one set up.
-  
+  const body = `### [🔎 Inspect this declaration suggestion](${url})
+
+- - -
+
 Bots should take care of checking the formatting and the validity of the declaration. As a human reviewer, you should check:
 
 ${checkBoxes.join('\n')}
@@ -69,6 +71,11 @@ ${checkBoxes.join('\n')}
 If no document type seems appropriate for this document yet it is relevant to track in this instance, please check if there is already an [open discussion](https://github.com/ambanum/OpenTermsArchive/discussions) about such a type and reference your case there, or open a new discussion if not.
 
 Thanks to your work and attention, Open Terms Archive will ensure that high quality data is available for all reusers, enabling them to do their part in shifting the balance of power towards end users and regulators instead of spending time collecting and cleaning documents 💪
+
+- - -
+
+_This suggestion has been created through the [Contribution Tool](https://github.com/OpenTermsArchive/contribution-tool/), which enables graphical declaration of documents.
+You can load it [on your local instance](${localUrl}) if you have one set up._
 `;
 
   try {
