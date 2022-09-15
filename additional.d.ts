8e1f8358d0aa2a5a65553207c3b3a1df3a9fa464
Martin Ratinaud <martinratinaud@gmail.com>
2022-09-15 15:22:47 +0400

    Use temp branch for OTA for exposing page-declaration

--- additional.d.ts
+++ additional.d.ts
@@ -2,7 +2,7 @@ type WithClassname<T = {}> = T & { className?: string };
 
 declare module 'open-terms-archive/fetch';
 declare module 'open-terms-archive/filter';
-declare module 'open-terms-archive/document-declaration';
+declare module 'open-terms-archive/page-declaration';
 
 declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGSVGElement>>;
