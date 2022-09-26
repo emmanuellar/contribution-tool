c35a766c9ae0fb0124ed1e8a659677ba7c157354
Martin Ratinaud <martinratinaud@gmail.com>
2022-09-26 17:05:08 +0400

    Refactor to factorize text to be put in PR comments

--- src/modules/Contribute/managers/ServiceManager.ts
+++ src/modules/Contribute/managers/ServiceManager.ts
@@ -10,6 +10,19 @@ export const deriveIdFromName = (name: string) => {
     .replace(/(&|\\|\/|:)/gi, '-'); // remove characters that might be problematic on the file system
 };
 
+const selectorsCheckboxes = [
+  '- [ ] **Selectors are:**',
+  '  - **stable**: as much as possible, the CSS selectors are meaningful and specific (e.g. `.tos-content` rather than `.ab23 .cK_drop > div`).',
+  '  - **simple**: the CSS selectors do not have unnecessary specificity (e.g. if there is an ID, do not add a class or a tag).',
+];
+
+const versionCheckboxes = [
+  '- [ ] **Generated version** is:',
+  '  - **relevant**: it is not just a series of links, for example.',
+  '  - **readable**: it is complete and not mangled.',
+  '  - **clean**: it does not contain navigation links, unnecessary images, or extra content.',
+];
+
 export const addOrUpdateService = async ({
   destination,
   name,
@@ -66,24 +79,14 @@ export const addService = async ({
   const branchName = snakeCase(prTitle);
 
   const hasSelector = !!json?.documents[documentType]?.select;
-  const selectorsCheckBoxes = hasSelector
-    ? [
-        '- [ ] **Selectors are:**',
-        '  - **stable**: as much as possible, the CSS selectors are meaningful and specific (e.g. `.tos-content` rather than `.ab23 .cK_drop > div`).',
-        '  - **simple**: the CSS selectors do not have unnecessary specificity (e.g. if there is an ID, do not add a class or a tag).',
-      ]
-    : [];
 
   const checkBoxes = [
     '- [ ] The suggested document **matches the scope of this instance**: it targets a service in the language, jurisdiction, and industry that are part of those [described](../#scope) for this instance.',
     `- [ ] **The service name \`${name}\` matches what you see on the web page**, and it complies with the [guidelines](https://github.com/OpenTermsArchive/contrib-declarations/blob/main/CONTRIBUTING.md#service-name).`,
     `- [ ] **The service ID \`${id}\` (i.e. the name of the file) is derived from the service name** according to the [guidelines](https://github.com/OpenTermsArchive/contrib-declarations/blob/main/CONTRIBUTING.md#service-id).`,
     `- [ ] The document type \`${documentType}\` is appropriate for this document: if you read out loud the [document type tryptich](https://github.com/ambanum/OpenTermsArchive/blob/main/src/archivist/services/documentTypes.json), you can say that **“this document describes how the \`writer\` commits to handle the \`object\` for its \`audience\`”**.`,
-    ...selectorsCheckBoxes,
-    '- [ ] **Generated version** is:',
-    '  - **relevant**: it is not just a series of links, for example.',
-    '  - **readable**: it is complete and not mangled.',
-    '  - **clean**: it does not contain navigation links, unnecessary images, or extra content.',
+    ...(hasSelector ? selectorsCheckboxes : []),
+    ...versionCheckboxes,
   ];
 
   const body = `### [🔎 Inspect this declaration suggestion](${url})
